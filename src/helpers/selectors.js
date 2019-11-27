eb9f4ee20a6c70dac98bee1f1de2073578bcac34
Slav <slalev57@me.com>
2019-11-27 15:59:21 -0500

    retrieving appointments using axios

---
+++ src/helpers/selectors.js
@@ -0,0 +1,46 @@
+
+// export const getAppointmentsForDay = (state, day) => {
+
+export function getAppointmentsForDay(state, day) {
+
+
+
+  
+  const appointmentsId = state.days
+    .filter(e => e.name === day)
+    
+    .map(e => e.appointments)
+    
+    .reduce((acc, val) => acc.concat(val), []);
+
+  
+  const appointment = [];
+  appointmentsId.forEach(e => {
+  appointment.push(state.appointments[e]);
+  });
+
+  return appointment;
+} 
+
+
+
+export  function getInterview(state, interview) {
+  
+  if(!interview) {
+    return null;
+  } 
+  
+  else {
+    const student = interview.student;
+    
+    const interviewer = state.interviewers[interview.interviewer];
+    
+    const interviewObject = { student, interviewer};
+    
+    console.log(interviewObject)
+    
+    
+    return interviewObject;
+  }
+
+}
