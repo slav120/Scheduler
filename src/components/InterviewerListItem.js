d13814b0f9733f2596911642bbcf39d9cc078373
Slav Lalev <53335999+slav120@users.noreply.github.com>
2019-12-04 18:24:38 -0500

    Update InterviewerListItem.js

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -18,11 +18,7 @@ return (
 
 <li className={intervieweritemClass} onClick={props.setInterviewer}>
   
-<img
- className="interviewers__item-image"
- src={props.avatar}
- alt={props.name}
-/>
+<img className="interviewers__item-image" src={props.avatar} alt={props.name} />
   
 {props.selected && props.name}
 
